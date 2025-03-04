db366acad5ab1cc2094277eac6f7ebed01ac5450
Hugo Morelli <hugo.morelli@navigaglobal.com>
2025-03-04 23:24:25 +0100

    [fix] remove Xcode warning

--- WallaMarvel/Data/Mappers/PaginatedResponseModel+DomainMapper.swift
+++ WallaMarvel/Data/Mappers/PaginatedResponseModel+DomainMapper.swift
@@ -9,12 +9,12 @@ import Foundation
 
 extension PaginatedResponseModel {
   typealias EntityType = PaginatedResponse
-  func toDomain<T>(dataType: T.Type) -> PaginatedResponse<T> {
-    return PaginatedResponse<T>(offset: offset,
+  func toDomain<U>(dataType: U.Type) -> PaginatedResponse<U> {
+    return PaginatedResponse<U>(offset: offset,
                                 limit: limit,
                                 total: total,
                                 count: count,
-                                results: (results ?? []).compactMap({$0.toDomain() as? T}
+                                results: (results ?? []).compactMap({$0.toDomain() as? U }
                                                                    )
     )
   }
