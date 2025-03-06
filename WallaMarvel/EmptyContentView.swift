380a2fe8d852cfbb0eacce1560ae5701181a05e7
Hugo Morelli <hugo.morelli@navigaglobal.com>
2025-03-06 23:52:50 +0100

    [feat] Update design of empty state

--- WallaMarvel/EmptyContentView.swift
+++ WallaMarvel/EmptyContentView.swift
@@ -20,15 +20,25 @@ final class EmptyContentView: UIView {
     let label = UILabel()
     label.textAlignment = .center
     label.numberOfLines = 0
-    label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
-    label.textColor = .gray
+    label.font = .systemFont(ofSize: 20, weight: .medium)
+    label.textColor = .label
     return label
+  }()
+  
+  private let emptyImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    imageView.layer.cornerRadius = 16
+    imageView.clipsToBounds = true
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.image = UIImage(named: "image")
+    return imageView
   }()
   
   private let reloadButton: UIButton = {
     let button = UIButton(type: .system)
     button.setTitle("Reload", for: .normal)
-    button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
     button.setTitleColor(.white, for: .normal)
     button.backgroundColor = .systemRed
     button.layer.cornerRadius = 8
@@ -53,6 +63,7 @@ final class EmptyContentView: UIView {
   
   private func setupUI() {
     addSubview(messageLabel)
+    addSubview(emptyImageView)
     addSubview(reloadButton)
     backgroundColor = .systemBackground
     messageLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -61,14 +72,18 @@ final class EmptyContentView: UIView {
     messageLabel.text = "Oops! Looks like your heroes are missing!. Please try again."
     
     NSLayoutConstraint.activate([
-      messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-      messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -20),
+      messageLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 30),
       messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
       messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
       
-      reloadButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 16),
+      emptyImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+      emptyImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+      emptyImageView.heightAnchor.constraint(equalToConstant: 300),
+      
+      reloadButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -30),
       reloadButton.centerXAnchor.constraint(equalTo: centerXAnchor),
-      reloadButton.widthAnchor.constraint(equalToConstant: 120),
+      reloadButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+      reloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
       reloadButton.heightAnchor.constraint(equalToConstant: 44)
     ])
     
